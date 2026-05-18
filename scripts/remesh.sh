4202236bb9785190a446c2ac7c21bf9eb6c71adb
guoyanting-hub <1528752579@qq.com>
2026-05-18 22:54:54 +0800

    Delete scripts/remesh.sh

--- scripts/remesh.sh
+++
@@ -1,7 +0,0 @@
-python data_preprocess/datagen_maps.py \
-    --dataroot '../dataset/Data_test/Data_simplify_best' \
-    --output '../dataset/Data_test/Data_maps' \
-
-# dataroot：Ther root for single tooth after simplification
-# output: Output root for data after remesh
-
