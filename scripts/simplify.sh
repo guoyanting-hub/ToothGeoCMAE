79df15d671f1877e09cb76ec13ec9482461ff97c
guoyanting-hub <1528752579@qq.com>
2026-05-18 22:55:12 +0800

    Delete scripts/simplify.sh

--- scripts/simplify.sh
+++
@@ -1,4 +0,0 @@
-python data_preprocess/simplify.py \
-    --dataroot '../dataset/Data_test/Data_manifold' \
-    --manifold '../dataset/Data_test/Data_manifold' \
-    --simplify '../dataset/Data_test/Data_simplify_best'
